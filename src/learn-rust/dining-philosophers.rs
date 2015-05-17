c18e82a851f6e08652a19da84384eed19de4e372
Brandon DeRosier <bdero@mit.edu>
2015-05-17 18:55:33 -0400

    Dining philosophers lineup

--- src/learn-rust/dining-philosophers.rs
+++ src/learn-rust/dining-philosophers.rs
@@ -1,1 +1,20 @@
-fn main() { }
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+}
+
+fn main() {
+    let p1 = Philosopher::new("Frantz Fanon");
+    let p2 = Philosopher::new("Thomas Sankara");
+    let p3 = Philosopher::new("Maria Mies");
+    let p4 = Philosopher::new("Che Guevara");
+    let p5 = Philosopher::new("Vladimir Lenin");
+    let p6 = Philosopher::new("Zak Cope");
+}
